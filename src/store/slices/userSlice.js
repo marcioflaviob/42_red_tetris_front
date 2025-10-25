6c078ddfaa59e37843b1c0f6ff65f095cff3e9a4
trimize <teorimize7@gmail.com>
2025-10-25 15:50:45 +0200

    restored file userslice

--- src/store/slices/userSlice.js
+++ src/store/slices/userSlice.js
@@ -1,20 +1,14 @@
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { createSlice } from '@reduxjs/toolkit';
 import { createUserService } from '../../services/UserService';
 
 const service = createUserService();
-
-export const fetchMatches = createAsyncThunk('user/fetchMatches', async () => {
-  const matches = await service.getMatches();
-  return matches;
-});
 
 const userSlice = createSlice({
   name: 'user',
   initialState: {
     username: service.getUsername(),
     avatar: service.getAvatar(),
-    matches: [],
-    matchesLoading: false,
+    matches: service.getMatches(),
   },
   reducers: {
     setUsername: (state, action) => {
@@ -26,23 +20,9 @@ const userSlice = createSlice({
       service.saveAvatar(action.payload);
     },
   },
-  extraReducers: (builder) => {
-    builder
-      .addCase(fetchMatches.pending, (state) => {
-        state.matchesLoading = true;
-      })
-      .addCase(fetchMatches.fulfilled, (state, action) => {
-        state.matches = action.payload;
-        state.matchesLoading = false;
-      })
-      .addCase(fetchMatches.rejected, (state) => {
-        state.matchesLoading = false;
-      });
-  },
 });
 
 export const { setUsername, setAvatar } = userSlice.actions;
 export const selectUsername = (state) => state.user.username;
 export const selectAvatar = (state) => state.user.avatar;
-export const selectMatches = (state) => state.user.matches;
 export default userSlice.reducer;
